a02515605a8c30577183a9b82158c910eb563cb3
ssvt <sergey.svyatoha@gmail.com>
2022-07-27 12:59:41 +0300

    12.1

---
+++ 2.JavaCore/src/com/javarush/task/task11/task1117/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task11.task1117;
+
+/* 
+Альтернативная цепочка наследования
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    //плотоядное животное
+    public class Carnivore extends Animal{
+
+    }
+    //корова
+    public class Cow extends Animal{
+
+    }
+
+    public class Dog extends Carnivore{
+
+    }
+    //свинья
+    public class Pig extends Animal{
+
+    }
+
+    public class Animal {
+
+    }
+}
