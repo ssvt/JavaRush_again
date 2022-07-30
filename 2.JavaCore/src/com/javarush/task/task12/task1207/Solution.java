870ed90173a748bdb52a953af3715d5f35a1097e
ssvt <sergey.svyatoha@gmail.com>
2022-07-30 12:28:18 +0300

    12.7

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        print(23);
+        print(Integer.getInteger("256"));
+    }
+
+    public static void print(int x) {
+
+    }
+
+    public static void print(Integer x){
+
+    }
+}
---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print(){
+
+    }
+
+    public static void print(int x){
+
+    }
+    public static void print(String str){
+
+    }
+    public static void print(Character ch){
+
+    }
+    public static void print(int x, int y){
+
+    }
+}
