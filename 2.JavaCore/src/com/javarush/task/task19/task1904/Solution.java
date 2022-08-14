25d4c583106e9fb6305e99446273adafe561058e
ssvt <sergey.svyatoha@gmail.com>
2022-08-14 21:24:13 +0300

    19.4

--- 2.JavaCore/src/com/javarush/task/task19/task1904/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1904/Solution.java
@@ -1,10 +1,12 @@
 package com.javarush.task.task19.task1904;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.Scanner;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.*;
 
 /* 
 И еще один адаптер
@@ -12,11 +14,41 @@ import java.util.Scanner;
 
 public class Solution {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+/*        PersonScannerAdapter psr = new PersonScannerAdapter(new Scanner(new FileReader("D:\\COMMON\\OUT\\in.txt")));
+        System.out.println(psr.read().toString());*/
     }
 
-    public static class PersonScannerAdapter {
+    public static class PersonScannerAdapter implements PersonScanner{
+        private Scanner fileScanner;
 
+        public PersonScannerAdapter(Scanner fileScanner) {
+            this.fileScanner = fileScanner;
+        }
+
+        // 0        1       2           3   4   5
+        //Иванов    Иван    Иванович    31  12  1950
+        @Override
+        public Person read() throws IOException {
+            String line = fileScanner.nextLine();
+//            String line = "Иванов Иван Иванович 31 12 1950";
+            String[] strArray = line.split(" ");
+
+            LocalDate ld = LocalDate.of(Integer.parseInt(strArray[5].trim()), Integer.parseInt(strArray[4].trim()), Integer.parseInt(strArray[3].trim()));
+            Date date = null;
+            try {
+                date = new SimpleDateFormat("yyyy-MM-dd").parse(ld.toString());
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
+            Person person = new Person(strArray[1], strArray[2], strArray[0], date);
+            return person;
+        }
+
+        @Override
+        public void close() throws IOException {
+            fileScanner.close();
+        }
     }
 }
