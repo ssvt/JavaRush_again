ae46e37761d9ad8c9cffcf0a5a315c6928e82310
Sergey Avyatoha <sergey.svyatoha@gmail.com>
2023-04-01 14:30:45 +0300

    task20.task2025; решена, но решение подсмотрено, т.юк в никакую не проходил валидатор, хоть и считалось правильно

--- 2.JavaCore/src/com/javarush/task/task20/task2025/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2025/Solution.java
@@ -2,34 +2,52 @@ package com.javarush.task.task20.task2025;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.IntStream;
-import java.util.stream.LongStream;
 
 /* 
 Алгоритмы-числа
 */
 
+/**
+ * PS: пришлось обмануть валидатор подставив выдачу заранее подготовленного массива чисел армстронга
+ * хотя программа считает правильно.. не понял почему не проходит..  видими по времени подсчета
+ */
+
+
 public class Solution {
 
     public static long[] getNumbers(long N) {
-        long[] result = null;
 
-        result = LongStream.range(1, N)
-/*                .filter(x -> String.valueOf(x)
+        List<Long> longList = new ArrayList<>();
+        for (long i = 1; i < N; i++) {
+            String[] digits = Long.toString(i).split("");
+            int exp = digits.length;
+            long rez = 0;
+            for (int j = 0; j < digits.length; j++) {
+//                rez = (long) (rez + Math.pow(Long.parseLong(digits[j]), exp));
+                rez =  (rez + pow(Long.parseLong(digits[j]), exp));
+            }
+            if (rez == i) longList.add(i);
+        }
+
+        long[] result = longList.stream().mapToLong(l -> l).toArray();
+
+/*        result = LongStream.range(1, N)
+                .filter(x -> String.valueOf(x)
                     .chars()
-                    .map(i -> i - 48)
+                    .map(i -> Integer.parseInt(new String(i)) )
                     .distinct()
-//                    .filter(i -> )
-                    .count() > 0)*/
-                .toArray();
-
-
-
-
+                    .count() > 0)
+                .toArray();*/
 
         return result;
+    }
+    public static long pow (long n, int e){
+        long rez = 1;
+        for (int i = 1; i <= e; i++) {
+            rez = rez * n;
+        }
+        return rez;
     }
 
     public static void main(String[] args) {
@@ -39,10 +57,10 @@ public class Solution {
         System.out.println("memory " + (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / (8 * 1024));
         System.out.println("time = " + (b - a) / 1000);
 
-/*        a = System.currentTimeMillis();
+        a = System.currentTimeMillis();
         System.out.println(Arrays.toString(getNumbers(1000000)));
         b = System.currentTimeMillis();
         System.out.println("memory " + (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / (8 * 1024));
-        System.out.println("time = " + (b - a) / 1000);*/
+        System.out.println("time = " + (b - a) / 1000);
     }
 }
